ef097a2fa37cf48b1a00d3f476f995cfd27dcf61
AtilaRajkov <atilar777@gmial.com>
2020-08-28 15:51:08 +0200

    Prepopulated users and questions tabels using model factories and seeder

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Question;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+  return [
+    'title' => $title = rtrim($faker->sentence(rand(5, 10)), '.'),
+    //'slug' => Str::slug($title),
+    'body' => $faker->paragraphs(rand(3, 7), true),
+    'views' => rand(0, 10),
+    'answers' => rand(0, 10),
+    'votes' => rand(-3, 10),
+    //'user_id' => \App\User::all()->random()->id
+  ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,21 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // $this->call(UserSeeder::class);
+    factory(App\User::class, 3)->create()
+      ->each(function($u) {
+        $u->questions()
+          ->saveMany(
+            factory(App\Question::class, rand(1, 5))->make()
+          );
+      });
+    //factory(App\Question::class, 20)->create();
+  }
 }
